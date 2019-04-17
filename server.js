be11bf3eaacab53905da6558b49a1a75ac2eee28
Yohane <1837512926@qq.com>
2019-04-17 19:25:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,94 +1,110 @@
-var express = require('express'); 
-var app = express();
-var http = require('http')
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
+// let a  = 1231
 
-// app.use('/', express.static(__dirname + '/public')); 
 
 app.use(express.static(__dirname+'/',{index:'index.html'})); 
 
-var port = 80
+const port = 2333
 
 
-app.use(
-	bodyParser.urlencoded({extended: true ,limit: '10mb'})
-)
-
-app.post('/uploadFile', (request, response)=>{
-    response.header('Access-Control-Allow-Origin', '*')
-    
-    // parse a file upload
-    var mime = require('mime');
-    var formidable = require('formidable');
-    var util = require('util');
-
-    var form = new formidable.IncomingForm();
-
-    var dir = !!process.platform.match(/^win/) ? '\\uploads\\' : '/uploads/';
-
-    form.uploadDir = __dirname + dir;
-    form.keepExtensions = true;
-    form.maxFieldsSize = 10 * 1024 * 1024;
-    form.maxFields = 1000;
-    form.multiples = false;
-
-    
-function getHeaders(opt, val) {
-    try {
-        var headers = {};
-        headers["Access-Control-Allow-Origin"] = "https://secure.seedocnow.com";
-        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-        headers["Access-Control-Allow-Credentials"] = true;
-        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-
-        if (opt) {
-            headers[opt] = val;
-        }
-
-        return headers;
-    } catch (e) {
-        return {};
-    }
-}
-    form.parse(request, function(err, fields, files) {
-        var file = util.inspect(files);
-        
-
-        // console.log(file.split('path:'))
-
-        var ar = file.match(/name\:(.+)\,/)
-        console.log(ar[1])
-
-        response.writeHead(200, getHeaders('Content-Type', 'application/json'));
-				
-        var fileName = file.split('path:')[1].split('\',')[0].split(dir)[1].toString().replace(/\\/g, '').replace(/\//g, '');
-        // var fileURL = 'http://' + app.address + ':' + port + '/uploads/' + fileName;
-
-        // var fileURL = 'http://' + 'localhost:80' + '/uploads/' + fileName;
-        var fileURL = 'http://' + 'localhost:80' + '/uploads/' + ar[1].trim().replace(/\'/g,'')
+app.use( bodyParser.urlencoded({extended: true ,limit: '10mb'}) )
 
 
-        console.log('fileURL: ', fileURL);
+console.log(`listen at ${port}`)
+server.listen(port)
 
-        //console.log(app)
-        response.write(JSON.stringify({
-            fileURL: fileURL
-        }));
-        response.end();
-    });
- 
+
+app.post('/uploadFile/:userid', (request, response)=>{
+
+	// console.log(`attr 2:${socket}`)
+	response.header('Access-Control-Allow-Origin', '*')
+
+	console.log(`userid:${request.params.userid}`)
+
+	// parse a file upload
+	var mime = require('mime');
+	var formidable = require('formidable');
+	var util = require('util');
+
+	var form = new formidable.IncomingForm();
+
+	var dir = !!process.platform.match(/^win/) ? '\\uploads\\' : '/uploads/';
+
+	form.uploadDir = __dirname + dir;
+
+	//console.log(`uploadDir:${form.uploadDir}`)
+	//console.log(`request:${request}`)
+	form.keepExtensions = true;
+	form.maxFieldsSize = 10 * 1024 * 1024;
+	form.maxFields = 1000;
+	form.multiples = false;
+
+
+			
+	function getHeaders(opt, val) {
+			try {
+					var headers = {};
+					headers["Access-Control-Allow-Origin"] = "https://secure.seedocnow.com";
+					headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+					headers["Access-Control-Allow-Credentials"] = true;
+					headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+					headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+
+					if (opt) {
+							headers[opt] = val;
+					}
+
+					return headers;
+			} catch (e) {
+					return {};
+			}
+	}
+
+
+	form.parse(request, function(err, fields, files) {
+			const file = util.inspect(files);
+			
+			// console.log(file.split('path:'))
+
+			// var ar = file.match(/name\:(.+)\,/)
+			//console.log(ar[1])
+
+			response.writeHead(200, getHeaders('Content-Type', 'application/json'));
+			
+			const fileName = file.split('path:')[1].split('\',')[0].split(dir)[1].toString().replace(/\\/g, '').replace(/\//g, '');
+			const fileURL = 'http://' + 'localhost' + ':' + port + '/uploads/' + fileName;
+
+			response.write(JSON.stringify({
+				status:'1',
+				message:'success!!!!!',
+				data:{
+					userid:request.params.userid+'@@@@@',
+					fileURL
+				}
+
+			}))
+			response.end()
+
+
+			io.emit('server-client:uploadfile',{
+				userid:request.params.userid,
+				fileURL
+			})
+
+	});
+
 })
 
-server.listen(port);
-console.log( `at ${port}!`)
 
 io.on("connection",function(socket){
-    console.log("a user connected");
+		console.log("a user connected");
 
     socket.on("disconnect",function(){
         console.log("a user disconnect");
@@ -97,19 +113,20 @@ io.on("connection",function(socket){
         });
     });
 
+		
     socket.on("client-server:join",function(data){
-        var userid = data.userid;
-        socket.name = userid;
+
+				socket.name = data.userid;
+
         io.emit("server-client:join",{
             msg : "join ok",
-            userid : userid
+            userid:data.userid
         });
     });
 
-    socket.on("client-server:message",function(data){
-        io.emit("server-client:message",data);
-    });
+    // socket.on("client-server:message",function(data){
+    //     io.emit("server-client:message",data);
+    // });
 
 });
-
-
+// // debugger
